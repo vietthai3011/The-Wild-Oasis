b517533c930e9ae2f329026521e825610a0bd627
vietthai.3011 <ngovietthai3011@gmail.com>
2025-04-08 10:31:35 +0700

    Fix Path

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/LogOut";
+import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
